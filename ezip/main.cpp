74ce3bde7e25201eb599f1bad04707766160de2c
altakase <yusuke.takase@town.biz>
2020-04-09 14:38:32 +0900

    Initial Commit

---
+++ ezip/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ezip
+//
+//  Created by town42 on 2020/04/09.
+//  Copyright © 2020 ci-sales. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
